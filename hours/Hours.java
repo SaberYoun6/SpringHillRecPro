8a52c80827435db172092b7456c29eb03162bc07
Saberyoun6 <samuel.young.103@gmail.com>
2022-09-22 00:56:33 -0400

    add in files that where missing

---
+++ hours/Hours.java
@@ -0,0 +1,10 @@
+package hours;
+
+abstract class Hours{
+
+	public abstract  double dailyTotal(double out, double in);
+	public abstract  double dailyTotal(double out0, double in0, double out1, double in1);
+
+
+
+}
